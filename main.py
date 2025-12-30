3c85113f0edeea7b14a4233fe67886f49bb87571
Santiago Gil <santiagogil2w@gmail.com>
2025-12-30 19:25:13 +0100

    Uploading again main

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ import os
 
 app = func.FunctionApp()
 
-# =========================
-# FUNCIÓN PARA ARREGLAR TILDE
-# =========================
 def fix_encoding(text):
     if isinstance(text, str):
         try:
@@ -18,9 +15,6 @@ def fix_encoding(text):
             return text
     return text
 
-# =========================
-# BLOB TRIGGER
-# =========================
 @app.function_name(name="clean_csv_function")
 @app.blob_trigger(
     arg_name="inputblob",
@@ -31,28 +25,21 @@ def main(inputblob: func.InputStream):
 
     logging.info(f"Procesando archivo: {inputblob.name}")
 
-    # =========================
-    # LEER CSV DESDE RAW
-    # =========================
     df = pd.read_csv(
         BytesIO(inputblob.read()),
         encoding="latin1",
         dtype={"numero_suministro": str}
     )
 
-    # Arreglar tildes
     for col in df.select_dtypes(include="object").columns:
         df[col] = df[col].apply(fix_encoding)
 
-    # Eliminar filas completamente vacías
     df = df.dropna(how="all")
 
-    # Eliminar columnas que no sirven (si existen)
     for col in ["fecha", "_id"]:
         if col in df.columns:
             df = df.drop(columns=[col])
 
-    # numero_suministro → quitar separadores de miles
     if "numero_suministro" in df.columns:
         df["numero_suministro"] = (
             df["numero_suministro"]
@@ -60,19 +47,14 @@ def main(inputblob: func.InputStream):
             .astype(int)
         )
 
-    # energia → decimal a int
     if "energia" in df.columns:
         df["energia"] = (
             pd.to_numeric(df["energia"], errors="coerce")
             .astype(int)
         )
 
-    # Limpiar nombres de columnas
     df.columns = df.columns.str.strip().str.lower()
 
-    # =========================
-    # GUARDAR EN RESULTS
-    # =========================
     blob_service_client = BlobServiceClient.from_connection_string(
         os.environ["AzureWebJobsStorage"]
     )
